15651d1b38dd5b4d60d6588f4fc99062cd799a03
carvason <sonia.carvalho@ine.pt>
2026-05-27 13:23:39 +0000

    new file:   nace_ex1.py

---
+++ nace_ex1.py
@@ -0,0 +1,84 @@
+# %%
+#  importar dados
+# Question 1 — Import libraries and load environment variables
+import mlflow
+from dotenv import load_dotenv
+
+load_dotenv(override=True)
+print('fff')
+# %%%
+
+# %%
+# Question 2 — Load the dataset from s3
+import polars as pl
+
+df = pl.read_parquet("https://minio.lab.sspcloud.fr/projet-formation/diffusion/funathon/2026/project2/generation_None_temp08.parquet")
+
+print(df.head())
+print(f"Total rows: {len(df)}")
+
+# %%
+# Question 3 — Count unique NACE codes
+n_classes = df['code'].n_unique()
+print(f"Number of unique NACE codes: {n_classes}")
+
+# %%
+# 3.1 Question 1 — Split the dataset into train / validation / test sets
+from sklearn.model_selection import train_test_split
+
+train_df, tmp_df = train_test_split(df, test_size=0.30, random_state=42)
+val_df, test_df  = train_test_split(tmp_df, test_size=0.50, random_state=42)
+
+X_train, y_train = train_df["label"].to_numpy(), train_df["code"].to_numpy()
+X_val, y_val = val_df["label"].to_numpy(), val_df["code"].to_numpy()
+X_test, y_test = test_df["label"].to_numpy(), test_df["code"].to_numpy()
+
+print(f"Train: {len(train_df)} | Val: {len(val_df)} | Test: {len(test_df)}")
+
+# %%
+#  Question 2 — Encode the labels
+from sklearn.preprocessing import LabelEncoder
+
+encoder = LabelEncoder()
+encoder.fit(train_df['code'].to_numpy())
+
+# %%
+all_codes  = set(df['code'])
+train_codes = set(train_df['code'])
+missing = all_codes - train_codes
+
+if missing:
+    print(f"WARNING: {len(missing)} code(s) missing from training set: {missing}")
+else:
+    print(f"OK — all {len(all_codes)} codes appear in the training set.")
+
+# %%
+# Question 3 — Prepare the labels to use them with ttc
+
+from torchTextClassifiers.value_encoder import ValueEncoder
+
+value_encoder = ValueEncoder(label_encoder=encoder)
+
+# %%
+# 4.3 Question 1 — Train the tokenizer and inspect a sample
+from torchTextClassifiers.tokenizers import WordPieceTokenizer
+
+tokenizer = WordPieceTokenizer(vocab_size=5000, output_dim=10)
+tokenizer.train(X_train)
+
+print("Output tensor size:", tokenizer.tokenize(X_train[0]).input_ids.shape)
+print("Vocabulary size:", tokenizer.vocab_size)
+
+# Look at an example of tokenization
+print("Raw text", X_train[0])
+print(
+    "Tokens id:",
+    tokenizer.tokenize(X_train[0]).input_ids.squeeze(0)
+)
+print(
+    "Tokens:",
+    tokenizer.tokenizer.convert_ids_to_tokens(
+        tokenizer.tokenize(X_train[0]).input_ids.squeeze(0)
+    )
+)
+# %%
